e6327930ea0a7c4a8c98f1ea933fa1f4a346327a
Wockstarz <130925725+w0ckstarz@users.noreply.github.com>
2025-05-30 21:28:35 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,20 +1,17 @@
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-
-local player = Players.LocalPlayer
+local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
-local hrp = character:WaitForChild("HumanoidRootPart")
+local root = character:WaitForChild("HumanoidRootPart")
+local runService = game:GetService("RunService")
+local reach = 60
 
-local reachDistance = 60
-
-RunService.RenderStepped:Connect(function()
+runService.RenderStepped:Connect(function()
     local ball = workspace:FindFirstChild("Ball") 
     if not ball or not ball:IsA("BasePart") then return end
 
-    local distance = (ball.Position - hrp.Position).Magnitude
-    if distance <= reachDistance then
+    local distance = (ball.Position - root.Position).Magnitude
+    if distance <= reach then
 
-        firetouchinterest(hrp, ball, 0)
-        firetouchinterest(hrp, ball, 1)
+        firetouchinterest(root, ball, 0)
+        firetouchinterest(root, ball, 1)
     end
 end)
